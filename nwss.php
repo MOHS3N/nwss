f0dd43ac51d2cb892607110644f3a6cc1a0fb557
mohs3n <mohsenniyazdel@gmail.com>
2013-02-02 15:58:15 +0330

    add nwss.php

---
+++ nwss.php
@@ -0,0 +1,10 @@
+<?/**
+Plugin Name: NWSS
+Plugin URI: http://www.mohs3n.com/nwss
+Description: Free WordPress Slide Show Plugin
+Author: Mohsen Niyazdel.H
+Version: 0.1
+Author URI: http://www.niyazdel.com/
+*/
+ 
+?>
