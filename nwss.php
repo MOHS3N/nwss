8345fd225d84177323499c0b0b0e2840375e1bac
mohsen niyazdel <mohsenniyazdel@gmail.com>
2013-02-04 14:32:58 +0330

    Update nwss.php
    Edit Version 

--- nwss.php
+++ nwss.php
@@ -4,7 +4,7 @@ Plugin Name: NWSS
 Plugin URI: http://www.mohs3n.com/nwss
 Description: Free WordPress Slide Show Plugin
 Author: Mohsen Niyazdel.H
-Version: 0.1
+Version: 0.0.1
 Author URI: http://www.niyazdel.com/
 */
  
